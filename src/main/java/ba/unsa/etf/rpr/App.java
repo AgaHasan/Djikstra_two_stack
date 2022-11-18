4fceb5af1efe2b9d427e9e70ea2929f135793302
AgaHasan <bhasanagic1@etf.unsa.ba>
2022-11-18 22:55:43 +0100

    Dodati unit testovi za main metodu koji provjeravaju validaciju izraza te jedan integracioni test za kraj(:

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -10,8 +10,8 @@ public class App
      * Main - ulazna metoda konzolne aplikacije koja implementira djikstrin algoritam pomocu dva steka
      * @param args prima niz argumenata koji su aritmeticki izrazi
      */
-    public static void main( String[] args )
-    {
+    public static String main( String[] args ) {
+        Double rezultat = null;
         try {
             if (args.length == 0) throw new RuntimeException("Nijedan argument nije primljen");
             int operatora = 0;
@@ -19,36 +19,34 @@ public class App
             int otvorenihZagrada = 0;
             int zatvorenihZagrada = 0;
 
-            for(String arg : args){
+            for (String arg : args) {
                 operanada = 0;
                 operatora = 0;
                 otvorenihZagrada = 0;
                 zatvorenihZagrada = 0;
 
                 String[] simboli = arg.split(" ");
-                for(String simbol : simboli){
-                    if(simbol.equals("(")){
-                        otvorenihZagrada ++;
-                    }
-                    else if(simbol.equals(")")){
-                        zatvorenihZagrada ++;
-                    }
-                    else if(StringIsNumberEvaluator.isNumber(simbol) == true){
-                        operanada ++;
-                    }
-                    else if(simbol.equals("+") || simbol.equals("*") || simbol.equals("-") || simbol.equals("/") || simbol.equals("sqrt")){
-                        operatora ++;
-                    }
-                    else throw new RuntimeException("Ilegalan izraz");
+                for (String simbol : simboli) {
+                    if (simbol.equals("(")) {
+                        otvorenihZagrada++;
+                    } else if (simbol.equals(")")) {
+                        zatvorenihZagrada++;
+                    } else if (StringIsNumberEvaluator.isNumber(simbol) == true) {
+                        operanada++;
+                    } else if (simbol.equals("+") || simbol.equals("*") || simbol.equals("-") || simbol.equals("/") || simbol.equals("sqrt")) {
+                        operatora++;
+                    } else throw new RuntimeException("Ilegalan izraz");
                 }
-                if(otvorenihZagrada != zatvorenihZagrada || otvorenihZagrada != operatora) throw new RuntimeException("Ilegalan izraz");
-                Double rezultat = ExpressionEvaluator.evaluate(arg);
+                if (otvorenihZagrada != zatvorenihZagrada || otvorenihZagrada != operatora || operanada == 0)
+                    throw new RuntimeException("Ilegalan izraz");
+                rezultat = ExpressionEvaluator.evaluate(arg);
                 System.out.println("Rezultat je : " + rezultat);
             }
-        }
-        catch(RuntimeException e){
+        } catch (RuntimeException e) {
             System.out.println(e.getMessage());
+            return e.getMessage();
         }
-
+        String a = String.valueOf(rezultat);
+        return a;
     }
 }
--- src/test/java/ba/unsa/etf/rpr/AppTest.java
+++ src/test/java/ba/unsa/etf/rpr/AppTest.java
@@ -9,11 +9,67 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AppTest 
 {
     /**
-     * Provjera validacije ulaza, primjer iz zadatka //
+     * Provjera validacije ulaza, primjer iz zadatka // ( 1 + ( 5* 20))
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void primjerIzZadatka()
     {
-        assertTrue( true );
+        assertEquals("Ilegalan izraz", App.main(new String[]{"( 1 + ( 5* 20))"}));
     }
+
+    /**
+     * Provjera validacije ulaza, primjer bez operanada // ( + ( * ) )
+     */
+    @Test
+    public void primjer2()
+    {
+        assertEquals("Ilegalan izraz", App.main(new String[]{"( + ( * ) )"}));
+    }
+
+    /**
+     * Provjera validacije ulaza, primjer gdje nisu sve otvorene zagrade zatvorene // ( 1 + ( 1 + 1 )
+     */
+    @Test
+    public void primjer3()
+    {
+        assertEquals("Ilegalan izraz", App.main(new String[]{"( 1 + ( 1 + 1 )"}));
+    }
+
+    /**
+     * Provjera validacije ulaza, primjer gdje fali prva zagrada //  1 + ( 1 + 1 ) )
+     */
+    @Test
+    public void primjer4()
+    {
+        assertEquals("Ilegalan izraz", App.main(new String[]{" 1 + ( 1 + 1 ) )"}));
+    }
+
+
+    /**
+     * Provjera validacije ulaza, primjer gdje fali operator //  ( 1 ( 1 + 1 ) )
+     */
+    @Test
+    public void primjer5()
+    {
+        assertEquals("Ilegalan izraz", App.main(new String[]{"( 1 ( 1 + 1 ) )"}));
+    }
+
+    /**
+     * Provjera validacije ulaza, primjer gdje imamo karakter koji nije medju zadatim //  ( 1 ! ( 1 + 1 ) )
+     */
+    @Test
+    public void primjer6()
+    {
+        assertEquals("Ilegalan izraz", App.main(new String[]{"( 1 ! ( 1 + 1 ) )"}));
+    }
+
+    /**
+     * I konacno, za kraj, jedan integracijski test da osiguramo da je sef sretan buduci da ce nam osigurati 100% pokrivenost koda >.<
+     */
+    @Test
+    public void integration()
+    {
+        assertEquals("101.0", App.main(new String[]{"( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )"}));
+    }
+
 }
