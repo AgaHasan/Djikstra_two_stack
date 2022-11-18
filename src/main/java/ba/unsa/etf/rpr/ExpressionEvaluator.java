7a1496dbbab93de8a5c1e0312bbe419757c48f33
AgaHasan <bhasanagic1@etf.unsa.ba>
2022-11-18 21:35:13 +0100

    Implementirana metoda evaluate u klasi ExpressionEvaluator

--- src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
+++ src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Stack;
+
 /**
  * Klasa sa jednom javnom metodom 'evaluate'
  */
@@ -10,7 +12,31 @@ public class ExpressionEvaluator {
      * @return The value of the expression after evaluation as Double
      */
     public static Double evaluate(String expression){
+        String operator = "";
         Double result = 0.;
+        Stack<String> operatori = new Stack<String>();
+        Stack<Double> operandi = new Stack<Double>();
+        String[] simboli = expression.split(" ");
+        for(String simbol : simboli){
+            if(simbol.equals("("));
+            else if(simbol.equals("+")) operatori.push(simbol);
+            else if(simbol.equals("-")) operatori.push(simbol);
+            else if(simbol.equals("*")) operatori.push(simbol);
+            else if(simbol.equals("/")) operatori.push(simbol);
+            else if(simbol.equals("sqrt")) operatori.push(simbol);
+            else if(simbol.equals(")")){
+                operator = operatori.pop();
+                Double v = operandi.pop();
+                if(operator.equals("+")) v = operandi.pop() + v;
+                if(operator.equals("-")) v = operandi.pop() - v;
+                if(operator.equals("*")) v = operandi.pop() * v;
+                if(operator.equals("/")) v = operandi.pop() / v;
+                if(operator.equals("sqrt")) v = Math.sqrt(v);
+                operandi.push(v);
+            }
+            else operandi.push(Double.parseDouble(simbol));
+        }
+        result = operandi.pop();
         return result;
     }
 }
