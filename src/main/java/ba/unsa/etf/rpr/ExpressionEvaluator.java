e315692802279d4e12cb03cdc1cc7c4a72b14b86
AgaHasan <bhasanagic1@etf.unsa.ba>
2022-11-18 20:23:49 +0100

    Dodat prototip klase ExpressionEvaluator

---
+++ src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.rpr;
+
+/**
+ * Klasa sa jednom javnom metodom 'evaluate'
+ */
+public class ExpressionEvaluator {
+    /**
+     * Metoda koja prima aritmeticki izraz te vraca njegovu izracunatu vrijednost
+     * @param expression the expression to be evaluated in the form of a String object
+     * @return The value of the expression after evaluation as Double
+     */
+    public Double evaluate(String expression){
+        Double result = 0.;
+        return result;
+    }
+}
